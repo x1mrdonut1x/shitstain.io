6fc0baaf2fd0a16db9cd799c4558b64834f62d6c
Alex Niznik <alex@niznik.info>
2023-03-04 11:51:50 +0100

    feat: add idText

--- client/src/components/Enemy.ts
+++ client/src/components/Enemy.ts
@@ -7,19 +7,9 @@ import { Entity } from '../../../engine/entities/Entity';
 
 export class Enemy extends EngineEnemy {
   private damageTexts: Set<DamageText> = new Set();
-  public spritesContainer = new PIXI.Container();
 
   constructor(private stage: PIXI.Container, engine: GameEngine, x: number, y: number) {
     super(engine, x, y);
-
-    // DEBUG
-    const idText = new PIXI.Text(this.id);
-    idText.position.x = this.x;
-    idText.position.y = this.y;
-    idText.style.fontSize = 12;
-    this.spritesContainer.addChild(idText);
-
-    stage.addChild(this.spritesContainer);
   }
 
   public onCollide(entity: Entity) {
@@ -34,11 +24,6 @@ export class Enemy extends EngineEnemy {
     }
 
     super.onCollide(entity);
-  }
-
-  public destroy(): void {
-    this.spritesContainer.destroy();
-    super.destroy();
   }
 
   update(delta: number) {
